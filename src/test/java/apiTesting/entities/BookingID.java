422db470c00e91ca632161652f0591cec4cf80a6
Ancuta Stafie <ancuta.stafie@cgm.com>
2022-12-20 17:08:01 +0200

    .

--- src/test/java/apiTesting/entities/BookingID.java
+++ src/test/java/apiTesting/entities/BookingID.java
@@ -1,7 +1,12 @@
 package apiTesting.entities;
 
-public class BookingID
-{
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BookingID {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonProperty("bookingid")
     private int bookingid;
 
     public void setId(int bookingid) {
